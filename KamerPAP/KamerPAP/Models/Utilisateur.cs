122e37c9f7096103020a3c54ca9b5575c11ab4f0
francistek@yahoo.fr <francis.tekobou-feudjio@expaceo.com>
2020-06-15 23:30:23 +0200

    Ajout du projet web api

---
+++ KamerPAP/KamerPAP/Models/Utilisateur.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KamerPAP.Models
+{
+    public class Utilisateur
+    {
+        public int Id {get;set;}
+
+        public string Email{get;set;}
+
+        public string PassWord { get; set; }
+
+        public string NomUtilisateur { get; set; }
+
+        public bool IsAdmin { get; set; }
+
+    }
+}
