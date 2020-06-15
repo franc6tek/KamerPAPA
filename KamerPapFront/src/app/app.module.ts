15f890c44ef66e0783ad6bd43dea5dfdc3e37bb5
francistek@yahoo.fr <francis.tekobou-feudjio@expaceo.com>
2020-06-15 18:32:43 +0200

    Ajout du routing

--- KamerPapFront/src/app/app.module.ts
+++ KamerPapFront/src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InscriptionComponent } from './inscription/inscription.component';
 import { IdentificationComponent } from './identification/identification.component';
 import { AnnoncesViewComponent } from './annonces-view/annonces-view.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'annonces', component: AnnoncesViewComponent},
+  { path: 'auth/identification', component: IdentificationComponent},
+  { path: 'auth/inscription', component: InscriptionComponent},
+  { path: '', component: AnnoncesViewComponent},
+]
 
 @NgModule({
   declarations: [
@@ -16,7 +26,11 @@ import { AnnoncesViewComponent } from './annonces-view/annonces-view.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
