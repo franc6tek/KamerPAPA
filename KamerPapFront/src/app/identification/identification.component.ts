2a35e1fb087ba0627751ca2e587307959a98ce95
francistek@yahoo.fr <francis.tekobou-feudjio@expaceo.com>
2020-06-17 20:04:44 +0200

    Ajout du formulaire de connexion

--- KamerPapFront/src/app/identification/identification.component.ts
+++ KamerPapFront/src/app/identification/identification.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-identification',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IdentificationComponent implements OnInit {
 
-  constructor() { }
+  identificationForm: FormGroup;
+  errorMessage: string;
+
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthService,
+              private router: Router) {
+
+   }
 
   ngOnInit(): void {
+    this.initForm()
   }
 
+  initForm(){
+    this.identificationForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      passWord: ['', [Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)]]
+    });
+  }
+
+  onSubmit(){
+    const email = this.identificationForm.get('email').value;
+    const passWord = this.identificationForm.get('passWord').value;
+    this.authService.identification(email, passWord);
+    this.router.navigate(['creerAnnonce']);
+  }
 }
