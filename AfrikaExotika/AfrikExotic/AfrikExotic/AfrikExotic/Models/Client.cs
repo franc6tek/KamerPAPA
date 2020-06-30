a734147860b875634b7f5ab6f0471c7fc16f9ed6
francistek@yahoo.fr <francis.tekobou-feudjio@expaceo.com>
2020-06-30 11:38:30 +0200

    Ajout nouveau projet

---
+++ AfrikaExotika/AfrikExotic/AfrikExotic/AfrikExotic/Models/Client.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AfrikExotic.Models
+{
+    public class Client
+    {
+        private Guid idClient;
+        public Guid IdClient
+        {
+            get { return idClient; }
+            set
+            {
+                idClient = value;
+            }
+        }
+
+        private Guid nomUtilisateur;
+        public Guid NomUtilisateur
+        {
+            get { return nomUtilisateur; }
+            set
+            {
+                nomUtilisateur = value;
+            }
+        }
+
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+            }
+        }
+
+        private string adresse;
+        public string Adresse
+        {
+            get { return adresse; }
+            set
+            {
+                adresse = value;
+            }
+        }
+
+        private string codePostal;
+        public string CodePostal
+        {
+            get { return codePostal; }
+            set
+            {
+                codePostal = value;
+            }
+        }
+
+        private string phone;
+        public string Phone
+        {
+            get { return phone; }
+            set
+            {
+                phone = value;
+            }
+        }
+
+        private bool isAdmin;
+        public bool IsAdmin
+        {
+            get { return isAdmin; }
+            set
+            {
+                isAdmin = value;
+            }
+        }
+    }
+}
